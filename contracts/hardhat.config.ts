df32562c4f24917cc02bab3c6c9a08fc49523768
p0lemarch <reginald@hatamoto.be>
2023-06-10 07:54:46 +0200

    some additional packages

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL ||
+  "https://sepolia-mainnet.g.alchemy.com/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user1: {
+      default: 1,
+    },
+    user2: {
+      default: 2,
+    },
+  },
 };
 
 export default config;
