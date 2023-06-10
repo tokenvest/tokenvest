f9bcbf73ad0eb27d2757bf1bc6294b8b3fc28119
Quint Daenen <quint@daenen.email>
2023-06-10 08:12:12 +0200

    Fix missing private key.

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,18 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+import { ethers } from "ethers";
+
 require("dotenv").config();
 
-const SEPOLIA_RPC_URL =
-  process.env.SEPOLIA_RPC_URL ||
-  "https://sepolia-mainnet.g.alchemy.com/v2/your-api-key";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://sepolia-mainnet.g.alchemy.com/v2/your-api-key";
+let PRIVATE_KEY: string = "";
+if (!process.env.PRIVATE_KEY) {
+  console.error("Private key not set!");
+} else {
+  PRIVATE_KEY = process.env.PRIVATE_KEY;
+}
 
-let mnemonic: string = "";
+let MNEMONIC: string = "";
 if (!process.env.MNEMONIC) {
   console.error("Mnemonic not set!");
 } else {
-  mnemonic = process.env.MNEMONIC;
+  MNEMONIC = process.env.MNEMONIC;
 }
 
 const config: HardhatUserConfig = {
@@ -21,12 +26,12 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       chainId: 31337,
-      accounts: process.env.MNEMONIC ? { mnemonic: mnemonic } : {},
+      accounts: process.env.MNEMONIC ? { mnemonic: MNEMONIC } : {},
     },
     sepolia: {
       chainId: 11155111,
       url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: process.env.PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
   namedAccounts: {
