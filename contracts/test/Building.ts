8883755874fbcb15762fe605642af5cd0fb16485
p0lemarch <reginald@hatamoto.be>
2023-06-10 16:19:09 +0200

    building tests

--- contracts/test/Building.ts
+++ contracts/test/Building.ts
@@ -1,14 +1,33 @@
 import hre from "hardhat";
 import { Building } from "../typechain-types";
+import { ethers } from "ethers";
 import { getAllListings } from "../sdk/Listings";
 import { expect } from "chai";
+
+import { HardhatNetworkHDAccountsConfig } from "hardhat/types";
+
+const accounts = hre.config.networks.hardhat
+  .accounts as HardhatNetworkHDAccountsConfig;
+
+function wallet(index: number): ethers.HDNodeWallet {
+  return ethers.HDNodeWallet.fromMnemonic(
+    ethers.Mnemonic.fromPhrase(accounts.mnemonic),
+    `${accounts.path}/${index}`
+  );
+}
 
 describe("Builder", () => {
   let building: Building;
   before(async () => {
+    let deployer: ethers.Signer;
+    let deployerWallet: ethers.HDNodeWallet;
+    let signer: ethers.Signer;
+    let signerWallet: ethers.HDNodeWallet;
+
     const Building = await hre.ethers.getContractFactory("Building");
-    building = await Building.deploy("URI");
     const deployerAddress = (await hre.ethers.getSigners())[0].address;
+
+    building = await Building.deploy("URI", deployerAddress);
     const tx = await building.mint(
       deployerAddress,
       0,
