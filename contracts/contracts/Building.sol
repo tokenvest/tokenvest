525e5742985c1e83ddc3436214df6aa2ef8066f2
p0lemarch <reginald@hatamoto.be>
2023-06-10 07:55:03 +0200

    deploy script building.sol

--- contracts/contracts/Building.sol
+++ contracts/contracts/Building.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: TODO
 
-//TODO @Reginald: figure out a out-of-the-box multisig solution
-
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Building is ERC1155 {
-    constructor() ERC1155("URI") {
-    }
+    constructor(string memory _uri) ERC1155(_uri) {}
 }
