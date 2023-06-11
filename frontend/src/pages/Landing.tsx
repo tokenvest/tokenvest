70767481f9a318285d6e50fe9eeee59633ec439e
Buidlor <jefboone@gmail.com>
2023-06-11 10:32:36 +0200

    assets card update

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,5 +1,5 @@
-import ConnectWallet from '../components/ConnectWallet';
-import Navbar from '../components/Navbar';
+import ConnectWallet from "../components/ConnectWallet";
+import Navbar from "../components/Navbar";
 
 const Landing = () => {
   return (
@@ -79,14 +79,32 @@ const Landing = () => {
             <img src="/marketplace4.png" alt="Villa" />
           </figure>
           <div className="card-body bg-gray-900 text-white">
-            <h2 className="card-title">South Sky Villa</h2>
+            <h2 className="card-title">
+              South Sky Villa
+              <div className="badge badge-primary">100€</div>
+            </h2>
             <p>
-              South Sky Villa is a luxurious property offering panoramic views and modern
-              interiors. This desirable residence features spacious living areas, a
-              high-end kitchen, and private, comfortable bedrooms. Enjoy the perfect blend
-              of elegance, comfort, and natural beauty at South Sky Villa.
+              South Sky Villa is a luxurious property offering panoramic views
+              and modern interiors. This desirable residence features spacious
+              living areas, a high-end kitchen, and private, comfortable
+              bedrooms. Enjoy the perfect blend of elegance, comfort, and
+              natural beauty at South Sky Villa.
             </p>
+            <progress
+              className="progress progress-primary w-56 mt-5"
+              value="70"
+              max="100"
+            ></progress>
+            7000/10000 shares sold
             <div className="card-actions justify-end">
+              <div className="mr-4">
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white mt-2"
+                  id="shares"
+                  type="number"
+                  placeholder="Enter amount"
+                />
+              </div>
               <ConnectWallet showAddress={false} />
             </div>
           </div>
