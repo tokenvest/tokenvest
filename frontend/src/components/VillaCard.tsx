066d85728d895247d585c1e6d08a9008d10b293b
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-06-28 19:10:26 +0200

    villacard elements are now fetched from NFT contract's metadata

--- frontend/src/components/VillaCard.tsx
+++ frontend/src/components/VillaCard.tsx
@@ -1,5 +1,4 @@
 import { useContractRead, useContractWrite } from "wagmi";
-
 import { useAuth } from "../providers/auth.provider";
 import { useState, useEffect } from "react";
 import abiContract from "../abis/abiContract.json";
@@ -10,6 +9,12 @@ const VillaCard = () => {
   const contractAddress = "0x275767F80F7A2734710f46d8080eE2F9aB781Ec5";
   const { user } = useAuth();
   const [amount, setAmount] = useState(0);
+  const [imgUrl, setImgUrl] = useState("");
+  const [metaData, setMetaData] = useState({
+    name: "",
+    image: "",
+    description: "",
+  });
 
   const tokenURI = useContractRead({
     address: contractAddress,
@@ -22,10 +27,18 @@ const VillaCard = () => {
   //tokenURI.data =ipfs://QmfR5DzK9UMWBWBrPYxpZheSSiPd2jksoy5iZLv26m5tSD
   useEffect(() => {
     const fetchData = async () => {
-      const ipfsHash = (tokenURI.data as string).replace("ipfs://", "");
-      const gatewayUrl = `https://ipfs.io/ipfs/${ipfsHash}`;
-      const result = await axios.get(gatewayUrl);
-      console.log("result of fetching ipfs : ", result.data);
+      try {
+        const metaDataHash = (tokenURI.data as string).replace("ipfs://", "");
+        const gatewayUrl = `https://crimson-subjective-whale-633.mypinata.cloud/ipfs/${metaDataHash}`;
+        const mdResult = await axios.get(gatewayUrl);
+        console.log("result of fetching ipfs : ", mdResult.data);
+        const pictureHash = mdResult.data.image.replace("ipfs://", "");
+        const imgUrl = `https://crimson-subjective-whale-633.mypinata.cloud/ipfs/${pictureHash}`;
+        setImgUrl(imgUrl);
+        setMetaData(mdResult.data);
+      } catch (err) {
+        console.error("error fetching metadata: ", err);
+      }
     };
     fetchData();
   }, [tokenURI.data]);
@@ -75,22 +88,18 @@ const VillaCard = () => {
     <div className="display flex justify-center items-center gap-2 mt-20">
       <div className="card card-compact w-96  bg-gray-900 shadow-xl ">
         <figure>
-          <img src="/marketplace4.png" alt="Villa" />
+          <img src={imgUrl} alt="Villa" />
         </figure>
         <div className="card-body text-white">
           <h2 className="card-title">
-            South Sky Villa
+            {metaData && metaData.name}
             <div className="badge badge-primary">
               {price.isSuccess &&
                 "$" + ethers.formatEther(price.data as string)}
             </div>
           </h2>
           <p className="xs:text-xs sm:text-sm md:text-base ">
-            South Sky Villa is a luxurious property offering panoramic views and
-            modern interiors. This desirable residence features spacious living
-            areas, a high-end kitchen, and private, comfortable bedrooms. Enjoy
-            the perfect blend of elegance, comfort, and natural beauty at South
-            Sky Villa.
+            {metaData && metaData.description}
           </p>
           <progress
             className="progress progress-primary w-56 mt-5"
