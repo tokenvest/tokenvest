022a1b10cd82c2e4eb7d0cc8524ac9d0488e2205
Buidlor <89695544+Buidlor@users.noreply.github.com>
2023-07-28 22:42:50 +0200

    rainbowkit next auth

--- tokenvestNext/pages/signin.tsx
+++ tokenvestNext/pages/signin.tsx
@@ -1,55 +1,51 @@
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { signIn } from "next-auth/react";
-import { useAccount, useConnect, useSignMessage, useDisconnect } from "wagmi";
 import { useRouter } from "next/router";
 import { useAuthRequestChallengeEvm } from "@moralisweb3/next";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { signIn, useSession } from "next-auth/react";
+import { useAccount, useSignMessage, useNetwork } from "wagmi";
+import { useEffect } from "react";
 
 function SignIn() {
-  const { connectAsync } = useConnect();
-  const { disconnectAsync } = useDisconnect();
-  const { isConnected } = useAccount();
+  const { isConnected, address } = useAccount();
+  const { chain } = useNetwork();
+  const { status } = useSession();
   const { signMessageAsync } = useSignMessage();
-  const { requestChallengeAsync } = useAuthRequestChallengeEvm();
   const { push } = useRouter();
+  const { requestChallengeAsync } = useAuthRequestChallengeEvm();
 
-  const handleAuth = async () => {
-    if (isConnected) {
-      await disconnectAsync();
+  useEffect(() => {
+    const handleAuth = async () => {
+      const response = await requestChallengeAsync({
+        address: address as string,
+        chainId: chain!.id,
+      });
+      const { message } = response!;
+
+      const signature = await signMessageAsync({ message });
+
+      // redirect user after success authentication to '/user' page
+      const urlResponse = await signIn("moralis-auth", {
+        message,
+        signature,
+        redirect: false,
+        callbackUrl: "/user",
+      });
+      const { url } = urlResponse!;
+      /**
+       * instead of using signIn(..., redirect: "/user")
+       * we get the url from callback and push it to the router to avoid page refreshing
+       */
+      push(url!);
+    };
+    if (status === "unauthenticated" && isConnected) {
+      handleAuth();
     }
-
-    const { account, chain } = await connectAsync({
-      connector: new MetaMaskConnector(),
-    });
-
-    const response = await requestChallengeAsync({
-      address: account,
-      chainId: chain.id,
-    });
-
-    const { message } = response!;
-
-    const signature = await signMessageAsync({ message });
-
-    // redirect user after success authentication to '/user' page
-    const urlResponse = await signIn("moralis-auth", {
-      message,
-      signature,
-      redirect: false,
-      callbackUrl: "/user",
-    });
-
-    const { url } = urlResponse!;
-    /**
-     * instead of using signIn(..., redirect: "/user")
-     * we get the url from callback and push it to the router to avoid page refreshing
-     */
-    push(url!);
-  };
+  }, [status, isConnected]);
 
   return (
     <div>
       <h3>Web3 Authentication</h3>
-      <button onClick={handleAuth}>Authenticate via Metamask</button>
+      <ConnectButton />
     </div>
   );
 }
